30f8482ed65712fa595ddc4ad358a84fdacd7e98
AntonGraf <ant0ngraf@yandex.ru>
2022-10-29 20:43:40 +0600

    Initial commit

---
+++ src/main/java/sky/pro/course3/homeworks/HomeworksApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course3/homeworks/HomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
