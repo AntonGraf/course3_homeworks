8545ba02cf5dba496762df090c149afdfbf54ceb
AntonGraf <ant0ngraf@yandex.ru>
2022-10-30 15:34:52 +0600

    Подключает swagger

--- src/main/java/sky/pro/course3/homeworks/HomeworksApplication.java
+++ src/main/java/sky/pro/course3/homeworks/HomeworksApplication.java
@@ -1,9 +1,11 @@
 package sky.pro.course3.homeworks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class HomeworksApplication {
 
     public static void main(String[] args) {
