6ab001600efef77e8bc87c6c0dcfd3209db0b1d2
AntonGraf <ant0ngraf@yandex.ru>
2022-11-03 11:22:28 +0600

    Добавляет интерфейсы репозиториев

---
+++ src/main/java/sky/pro/course3/homeworks/repository/FacultyRepository.java
@@ -0,0 +1,11 @@
+package sky.pro.course3.homeworks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sky.pro.course3.homeworks.model.Faculty;
+
+import java.util.Collection;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    Collection<Faculty> findFacultyByColor(String color);
+}
