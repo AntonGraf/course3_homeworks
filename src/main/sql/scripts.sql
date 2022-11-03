9a0623d7e09a79a8e69ff3bb6ed43b388ef03818
AntonGraf <ant0ngraf@yandex.ru>
2022-11-03 22:30:11 +0600

    Добавляет sql-скрипты

---
+++ src/main/sql/scripts.sql
@@ -0,0 +1,5 @@
+SELECT * FROM student WHERE age >= 10 AND age <= 20;
+SELECT name FROM student;
+SELECT * FROM student WHERE name LIKE lower('%о%') OR name LIKE upper('%о%');
+SELECT * FROM student WHERE age < id;
+SELECT * FROM student ORDER BY age;
