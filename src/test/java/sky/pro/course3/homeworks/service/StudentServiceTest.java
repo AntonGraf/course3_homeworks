9d3b77218ed56a5f4b8c7782945a126c3e933844
AntonGraf <ant0ngraf@yandex.ru>
2022-11-03 12:49:27 +0600

    Изменяет тесты для сервиса StudentService

--- src/test/java/sky/pro/course3/homeworks/service/StudentServiceTest.java
+++ src/test/java/sky/pro/course3/homeworks/service/StudentServiceTest.java
@@ -1,23 +1,33 @@
 package sky.pro.course3.homeworks.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sky.pro.course3.homeworks.model.Student;
+import sky.pro.course3.homeworks.repository.StudentRepository;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
-    private final StudentService out = new StudentService();
+    @Mock
+    private StudentRepository studentRepository;
+    @InjectMocks
+    private StudentService out;
 
-    @BeforeEach
-    void setUp() {
-        out.getAllStudent().clear();
+    StudentServiceTest() {
     }
 
     public static Stream<Arguments> provideParamsTest() {
@@ -32,8 +42,13 @@ class StudentServiceTest {
     @MethodSource("provideParamsTest")
     void createStudent(Student student) {
 
+        when(studentRepository.findAll()).thenReturn(List.of());
         assertTrue(out.getAllStudent().isEmpty());
+
+        when(studentRepository.save(student)).thenReturn(student);
         assertEquals(out.createStudent(student), student);
+
+        when(studentRepository.findAll()).thenReturn(List.of(student));
         assertEquals(out.getAllStudent().size(), 1);
         assertTrue(out.getAllStudent().contains(student));
     }
@@ -41,19 +56,22 @@ class StudentServiceTest {
     @Test
     void getStudent() {
 
+        when(studentRepository.findAll()).thenReturn(List.of());
         assertTrue(out.getAllStudent().isEmpty());
-        assertNull(out.getStudent(3L));
+        assertThrows(NoSuchElementException.class, () -> out.findStudent(3));
 
-        fillStudents();
+        List<Student> studentList = getStudents();
+        when(studentRepository.findAll()).thenReturn(studentList);
         assertEquals(out.getAllStudent().size(),4);
 
         Student exceptedStudent = new Student("Иван", 20);
-        exceptedStudent.setId(2);
-        assertEquals(out.getStudent(2L), exceptedStudent);
-        exceptedStudent.setId(1);
-        assertNotEquals(out.getStudent(2L), exceptedStudent);
+        exceptedStudent.setId(2L);
+        when(studentRepository.findById(2L)).thenReturn(Optional.ofNullable(studentList.get(1)));
+        assertEquals(out.findStudent(2L), exceptedStudent);
+        exceptedStudent.setId(1L);
+        assertNotEquals(out.findStudent(2L), exceptedStudent);
 
-        assertNull(out.getStudent(5L));
+        assertThrows(NoSuchElementException.class, () -> out.findStudent(5));
 
     }
 
@@ -61,12 +79,18 @@ class StudentServiceTest {
     @MethodSource("provideParamsTest")
     void updateStudent(Student student) {
 
+        when(studentRepository.findAll()).thenReturn(List.of());
         assertTrue(out.getAllStudent().isEmpty());
 
-        fillStudents();
+        List<Student> studentList = getStudents();
+        when(studentRepository.findAll()).thenReturn(studentList);
 
-        student.setId(2);
-        assertEquals(out.updateStudent(2L, student), student);
+        student.setId(2L);
+        when(studentRepository.save(student)).thenReturn(student);
+        studentList.get(1).setAge(student.getAge());
+        studentList.get(1).setName(student.getName());
+        assertEquals(out.editStudent(student), student);
+
         assertTrue(out.getAllStudent().contains(student));
 
     }
@@ -74,15 +98,14 @@ class StudentServiceTest {
     @Test
     void deleteStudent() {
 
+        when(studentRepository.findAll()).thenReturn(List.of());
         assertTrue(out.getAllStudent().isEmpty());
-        assertNull(out.deleteStudent(1L));
 
-        fillStudents();
-
+        when(studentRepository.findAll()).thenReturn(getStudents());
         int countStudents = out.getAllStudent().size();
-        Student exceptedStudent = new Student("Ольга", 23);
-        exceptedStudent.setId(3);
-        assertEquals(out.deleteStudent(3L), exceptedStudent);
+        out.deleteStudent(3L);
+
+        when(studentRepository.findAll()).thenReturn(getStudents().subList(0, getStudents().size() - 1));
         assertEquals(out.getAllStudent().size(), countStudents - 1);
 
 
@@ -91,23 +114,35 @@ class StudentServiceTest {
     @Test
     void getStudentByAge() {
 
+        when(studentRepository.findAll()).thenReturn(List.of());
         assertTrue(out.getAllStudent().isEmpty());
+        when(studentRepository.findStudentsByAge(20)).thenReturn(List.of());
         assertTrue(out.getStudentByAge(20).isEmpty());
 
-        fillStudents();
+        List<Student> studentList = getStudents();
+        when(studentRepository.findAll()).thenReturn(studentList);
 
         assertFalse(out.getAllStudent().isEmpty());
 
         Student exceptedStudent = new Student("Ольга", 23);
-        exceptedStudent.setId(3);
+        exceptedStudent.setId(3L);
+        when(studentRepository.findStudentsByAge(23)).thenReturn(List.of(exceptedStudent));
         assertTrue(out.getStudentByAge(23).contains(exceptedStudent));
         assertTrue(out.getAllStudent().containsAll(out.getStudentByAge(20)));
     }
 
-    public void fillStudents() {
-        out.createStudent(new Student("Сергей", 25));
-        out.createStudent(new Student("Иван", 20));
-        out.createStudent(new Student("Ольга", 23));
-        out.createStudent(new Student("Екатерина", 20));
+    public List<Student> getStudents() {
+        List<Student> studentList = List.of(
+                new Student("Сергей", 25),
+                new Student("Иван", 20),
+                new Student("Ольга", 23),
+                new Student("Екатерина", 20)
+        );
+
+        for (int i = 0; i < studentList.size(); i++) {
+            studentList.get(i).setId(i + 1);
+        }
+        return studentList;
+
     }
 }
