dca79b3e0bde21da96205b79c1672b592fd28f6a
AntonGraf <ant0ngraf@yandex.ru>
2022-11-09 10:04:40 +0600

    Добавляет репозиторий, сервис и контроллер аватар

---
+++ src/main/java/sky/pro/course3/homeworks/service/AvatarService.java
@@ -0,0 +1,66 @@
+package sky.pro.course3.homeworks.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import sky.pro.course3.homeworks.model.Avatar;
+import sky.pro.course3.homeworks.model.Student;
+import sky.pro.course3.homeworks.repository.AvatarRepository;
+import sky.pro.course3.homeworks.repository.StudentRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+@Service
+public class AvatarService {
+
+    private final AvatarRepository avatarRepository;
+    private final StudentRepository studentRepository;
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+
+    public AvatarService(AvatarRepository avatarRepository, StudentRepository studentRepository) {
+        this.avatarRepository = avatarRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+
+        Student student = studentRepository.findById(studentId).get();
+        Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+            bis.transferTo(bos);
+        }
+
+        Avatar avatar = findAvatar(studentId);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatarRepository.save(avatar);
+    }
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    public Avatar findAvatar(Long studentId) {
+        Avatar avatar = avatarRepository.findAvatarByStudentId(studentId);
+
+        if (avatar == null) {
+            avatar = new Avatar();
+        }
+
+        return avatar;
+    }
+}
